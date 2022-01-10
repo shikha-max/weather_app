34c66b5c1e6817f15c34c2c101d8f0df56302463
shikha-max <tshikha66@mail.com>
2022-01-10 18:46:24 +0530

    updated

---
+++ weather/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div class="sun">
+      <div class="beam"> </div>
+      <div class="beam"> </div>
+      <div class="beam"> </div>
+      <div class="beam"> </div>
+      <div class="beam"> </div>
+      <div class="beam"> </div>
+      <div class="beam"> </div>
+      <div class="beam"> </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
